24f24f8dc4ab2b4b4e52b74bbd8f698e95c5b5dd
Valerie Vozza <valerievozza@gmail.com>
2022-04-14 21:55:38 -0700

    add question and answer - answer validation not functional

--- js/main.js
+++ js/main.js
@@ -9,8 +9,22 @@
       .then(data => {
         console.log(data.results[0].question)
         console.log(data.results[0].correct_answer)
+        document.querySelector('h2').innerText = data.results[0].question
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 //}
+
+document.querySelector('button').addEventListener('click', submitAnswer)
+
+function submitAnswer() {
+    let userAnswer = document.querySelector('input').value
+    let correctAnswer = data.results[0].correct_answer
+    
+    if (userAnswer === correctAnswer) {
+        console.log('Correct!')
+    } else {
+        console.log('Nope, wrong!')
+    }
+}
