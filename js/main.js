27e7f428dc9275f641d31309c0348fb66faa8567
Valerie Vozza <valerievozza@gmail.com>
2022-04-15 20:39:26 -0700

    fixed scope issue

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@
       .then(data => {
         console.log(data.results[0].question)
         console.log(data.results[0].correct_answer)
+        document.querySelector('button').addEventListener('click', submitAnswer)
+        function submitAnswer() {
+            let userAnswer = document.querySelector('input').value
+            let correctAnswer = data.results[0].correct_answer
+    
+            if (userAnswer === correctAnswer) {
+                console.log('Correct!')
+            } else {
+                console.log('Nope, wrong!')
+            }
+        }
         document.querySelector('h2').innerText = data.results[0].question
       })
       .catch(err => {
@@ -16,15 +27,3 @@
       });
 //}
 
-document.querySelector('button').addEventListener('click', submitAnswer)
-
-function submitAnswer() {
-    let userAnswer = document.querySelector('input').value
-    let correctAnswer = data.results[0].correct_answer
-    
-    if (userAnswer === correctAnswer) {
-        console.log('Correct!')
-    } else {
-        console.log('Nope, wrong!')
-    }
-}
