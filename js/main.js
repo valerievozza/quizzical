8230effc8aecd6cea2c2374a09e05a4d99e2d6eb
Valerie Vozza <valerievozza@gmail.com>
2022-04-15 21:09:10 -0700

    added local storage to keep score

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//document.querySelector('button').addEventListener('click', getFetch)
 
 //function getFetch(){
 //  const numOfQuestions = document.querySelector('input').value
@@ -11,13 +10,16 @@
         console.log(data.results[0].correct_answer)
         document.querySelector('button').addEventListener('click', submitAnswer)
         function submitAnswer() {
-            let userAnswer = document.querySelector('input').value
-            let correctAnswer = data.results[0].correct_answer
+            let userAnswer = document.querySelector('input').value.toLowerCase()
+            let correctAnswer = data.results[0].correct_answer.toLowerCase()
     
             if (userAnswer === correctAnswer) {
                 console.log('Correct!')
+                addScore()
+                document.querySelector('h3').innerText = 'Correct!'
             } else {
                 console.log('Nope, wrong!')
+                document.querySelector('h3').innerText = 'Nope!'
             }
         }
         document.querySelector('h2').innerText = data.results[0].question
@@ -25,5 +27,27 @@
       .catch(err => {
           console.log(`error ${err}`)
       });
+      
+function addScore(){
+    scoreVal = Number(localStorage.getItem('score'))
+    scoreVal += 1
+    localStorage.setItem('score', scoreVal)
+}
 //}
+
+/*document.getElementById('category').addEventListener('click', getCategory)
+const categoies = 'https://opentdb.com/api_category.php'
+
+function getCategory() {
+    
+    fetch(category)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}*/
+
 
