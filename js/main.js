7342a0de582be6021b6c6dc7d4b898929f8d8ffa
Valerie Vozza <valerievozza@gmail.com>
2022-04-15 21:19:27 -0700

    added next question button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+
+
 //function getFetch(){
 //  const numOfQuestions = document.querySelector('input').value
   const url = `https://opentdb.com/api.php?amount=10`
@@ -8,7 +10,7 @@
       .then(data => {
         console.log(data.results[0].question)
         console.log(data.results[0].correct_answer)
-        document.querySelector('button').addEventListener('click', submitAnswer)
+        document.getElementById('submit').addEventListener('click', submitAnswer)
         function submitAnswer() {
             let userAnswer = document.querySelector('input').value.toLowerCase()
             let correctAnswer = data.results[0].correct_answer.toLowerCase()
@@ -34,6 +36,35 @@ function addScore(){
     localStorage.setItem('score', scoreVal)
 }
 //}
+
+document.getElementById('next').addEventListener('click', nextQuestion)
+
+function nextQuestion () {
+    fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.results[0].question)
+        console.log(data.results[0].correct_answer)
+        document.getElementById('submit').addEventListener('click', submitAnswer)
+        function submitAnswer() {
+            let userAnswer = document.querySelector('input').value.toLowerCase()
+            let correctAnswer = data.results[0].correct_answer.toLowerCase()
+    
+            if (userAnswer === correctAnswer) {
+                console.log('Correct!')
+                addScore()
+                document.querySelector('h3').innerText = 'Correct!'
+            } else {
+                console.log('Nope, wrong!')
+                document.querySelector('h3').innerText = 'Nope!'
+            }
+        }
+        document.querySelector('h2').innerText = data.results[0].question
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
 
 /*document.getElementById('category').addEventListener('click', getCategory)
 const categoies = 'https://opentdb.com/api_category.php'
